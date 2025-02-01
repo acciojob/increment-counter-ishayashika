bc6ac5ae514e64328eb7009bc9d1303f85151c76
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-01 14:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let btn=document.getElementById("incrementBtn");
+btn.addEventListener("click", function(){
+	alert(counter.innerText);
+	counter.innerText=parseInt(counter.innerText)+1;
+})
